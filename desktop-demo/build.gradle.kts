e0846b08d3009e226dc47bb897b2b93786e00eb6
chaosannals <chaosannals@outlook.com>
2023-10-21 09:22:42 +0800

    打包方式。

--- desktop-demo/build.gradle.kts
+++ desktop-demo/build.gradle.kts
@@ -26,6 +26,10 @@ dependencies {
     api("moe.tlaster:precompose-molecule:$precomposeVersion") // For Molecule intergration
     api("moe.tlaster:precompose-viewmodel:$precomposeVersion") // For ViewModel intergration
     api("moe.tlaster:precompose-koin:$precomposeVersion") // For Koin intergration
+
+    val ktorVersion = "2.3.5"
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
 }
 
 compose.desktop {
@@ -33,7 +37,19 @@ compose.desktop {
         mainClass = "MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            // 每种格式都依赖特定的打包工具。
+            // gradle 的 compose desktop 分组下的 package* 任务
+            targetFormats(
+                // mac
+                TargetFormat.Dmg, // 不支持交叉编译，在 windows 提示 （packageDmg SKIPPED）跳过了。
+                //TargetFormat.Pkg, // 这个 windows 下会报错
+                // windows
+                TargetFormat.Msi, // 使用的 Wix 打包，会自动下载打包工具，要确保网络通畅。 安装后没 uninstall.exe 但是控制面板可以删除。。。。
+                TargetFormat.Exe, // 使用的 Wix 打包，会自动下载打包工具，要确保网络通畅。 安装后没 uninstall.exe 但是控制面板可以删除。。。。
+                // linux
+                TargetFormat.Deb, // 不支持交叉编译，在 windows 提示 （packageDeb SKIPPED）跳过了。
+                TargetFormat.Rpm, // 不支持交叉编译，在 windows 提示 （packageRpm SKIPPED）跳过了。
+            )
             packageName = "desktop-demo"
             packageVersion = "1.0.0"
         }
